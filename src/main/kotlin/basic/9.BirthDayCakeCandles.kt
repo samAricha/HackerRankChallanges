af49ca7e31242cf872d59e0c36f79cbf79cd9a54
samAricha <samaricha001@gmail.com>
2024-09-01 20:23:31 +0300

    Birthday Cake Candles problem

---
+++ src/main/kotlin/basic/9.BirthDayCakeCandles.kt
@@ -0,0 +1,30 @@
+package org.example.basic
+
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.text.*
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+fun birthdayCakeCandles(candles: Array<Int>): Int {
+    // Write your code here
+    val maxHeight = candles.maxOrNull() ?: 0
+
+    return candles.count{it -> it.equals(maxHeight)}
+
+}
+
+fun main(args: Array<String>) {
+    val candlesCount = readLine()!!.trim().toInt()
+
+    val candles = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = birthdayCakeCandles(candles)
+
+    println(result)
+}
