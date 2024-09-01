7be924fca783cc61c9f9dfeea6ece52df78893c3
samAricha <samaricha001@gmail.com>
2024-09-01 03:23:15 +0300

    staircase problem

---
+++ src/main/kotlin/basic/7.StairCase.kt
@@ -0,0 +1,48 @@
+package org.example.basic
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fun staircase(n: Int): Unit {
+    // Write your code here
+    for(i in 0 until n){
+        val start = n-1-i
+        for (j in 0 until n){
+            if (j < start){
+                print(" ")
+            }else{
+                print("#")
+            }
+        }
+        println()
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    staircase(n)
+}
