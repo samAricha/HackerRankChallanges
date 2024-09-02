1c8c9e377364e836d8d3fb06f15c58af8c5cd227
samAricha <samaricha001@gmail.com>
2024-09-02 14:16:43 +0300

    ApplesAndOrange problem

---
+++ src/main/kotlin/basic/12.AppleAndOrange.kt
@@ -0,0 +1,72 @@
+package org.example.basic
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'countApplesAndOranges' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER s
+ *  2. INTEGER t
+ *  3. INTEGER a
+ *  4. INTEGER b
+ *  5. INTEGER_ARRAY apples
+ *  6. INTEGER_ARRAY oranges
+ */
+
+fun countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: Array<Int>, oranges: Array<Int>): Unit {
+    //get house range
+    val houseRange = s..t
+    //get apple and orange distances in absolutes
+    val applesAbsolutes = apples.map{it+a}
+    val orangesAbsolutes = oranges.map{it+b}
+    //get number of apples and oranges within range
+    val appleCount = applesAbsolutes.filter { it in houseRange }.count()
+    val orangesCount = orangesAbsolutes.filter { it in houseRange }.count()
+
+    println(appleCount)
+    println(orangesCount)
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val s = first_multiple_input[0].toInt()
+
+    val t = first_multiple_input[1].toInt()
+
+    val second_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val a = second_multiple_input[0].toInt()
+
+    val b = second_multiple_input[1].toInt()
+
+    val third_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val m = third_multiple_input[0].toInt()
+
+    val n = third_multiple_input[1].toInt()
+
+    val apples = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val oranges = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
+}
